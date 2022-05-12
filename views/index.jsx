42b8cb50ed3951235bc7f3f25cb995c049b4c4c0
Mason Wagoner <mgwagoner@gmail.com>
2022-05-12 16:29:13 -0400

    sending index and show data plus bonus

--- views/index.jsx
+++ views/index.jsx
@@ -1,9 +1,9 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads}) {
+function Index ({breads, title}) {
   return (
-    <Default>
+    <Default title={title}>
       <h2>Index Page</h2>
       {/* <p>I have {breads[2].name} bread!</p> */}
       {/* The .map() function takes a callback as an argument. The callback requires a minimum of one argument. The first is the item in the array we are iterating over. The second argument is optional and will be the index position of the array item. */}
